6c20fecfa458ece8686115f0a76fdaba905e79b1
Matheus Maia <matheusmaiadasilva93@gmail.com>
2020-08-07 01:02:42 -0300

    Add solution project

---
+++ EasyBar/Controllers/AuthenticationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyBar.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        public AuthenticationController()
+        {
+
+        }
+    }
+}
---
+++ EasyBar/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EasyBar.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {      
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "I am API EasyBar";
+        }
+    }
+}
