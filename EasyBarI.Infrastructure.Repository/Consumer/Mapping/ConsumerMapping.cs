9fe92bd6d2ea9d66d9fce7d32d0fac3375399f04
Matheus Maia <matheusmaiadasilva93@gmail.com>
2020-08-09 22:29:31 -0300

    Adicionando referencias das entidades consumer e order no mapping

--- EasyBarI.Infrastructure.Repository/Consumer/Mapping/ConsumerMapping.cs
+++ EasyBarI.Infrastructure.Repository/Consumer/Mapping/ConsumerMapping.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EasyBar.Domain.Entity.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace EasyBarI.Infrastructure.Repository.Consumer.Mapping
 {
     public class ConsumerMapping : IEntityTypeConfiguration<ConsumerEntity>
     {
+        public void Configure(EntityTypeBuilder<ConsumerEntity> builder)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- EasyBarI.Infrastructure.Repository/Order/Mapping/OrderMapping.cs
+++ EasyBarI.Infrastructure.Repository/Order/Mapping/OrderMapping.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EasyBar.Domain.Entity.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace EasyBarI.Infrastructure.Repository.Order.Mapping
 {
     public class OrderMapping : IEntityTypeConfiguration<OrderEntity>
     {
+        public void Configure(EntityTypeBuilder<OrderEntity> builder)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
