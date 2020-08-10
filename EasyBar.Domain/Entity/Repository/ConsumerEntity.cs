e152545748a11f696c76b5efb4399dafcf931a31
Pedro Fiuza <pdrfiuza@gmail.com>
2020-08-09 22:28:16 -0300

    ConsumerEntity and OrderEntity

---
+++ EasyBar.Domain/Entity/Repository/ConsumerEntity.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyBar.Domain.Entity.Repository
+{
+    public class ConsumerEntity : EntityBase
+    {
+      
+    }
+}
--- EasyBar.Domain/Entity/Repository/OrderEntity.cs
+++ EasyBar.Domain/Entity/Repository/OrderEntity.cs
@@ -6,7 +6,30 @@ namespace EasyBar.Domain.Entity.Repository
 {
     public class OrderEntity : EntityBase
     {
-        
+
+        public string Item { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Value { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        //Key region
+
+        public ConsumerEntity Consumer { get; set; }
+
+        //End Key region
+
+        public OrderEntity(string Item, int Quantity, decimal Value) {
+            this.Item = Item;
+            this.Quantity =  Quantity;
+            this.Value = Value;
+            this.CreatedAt = DateTime.Now;
+            this.UpdatedAt = DateTime.Now;
+        }
 
     }
 }
